3e089fda8b28fc5e493deed143494f89c2cbac0b
Ryan Tucker <nick.tucker.bass@gmail.com>
2021-08-18 15:31:13 -0400

    deployed to firebass (lol)

--- src/components/pages/Videos.jsx
+++ src/components/pages/Videos.jsx
@@ -6,6 +6,7 @@ const Videos = ( ) => {
 
       <div className="mainDiv">
         <iframe width="560" height="315" src="https://www.youtube.com/embed/Qth8pswAYS4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/AFQ1WNBvdgo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/4SITpvY0C74" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/wMuUVh6-kNQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/OIddSdod4o0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
