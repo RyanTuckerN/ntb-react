477b97a5e4b05aa48658bc7856a7100e6818f453
Ryan Tucker <nick.tucker.bass@gmail.com>
2021-08-29 20:43:33 -0400

    updated App.js

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import Footer from './components/Footer'
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
     <div className="App">
